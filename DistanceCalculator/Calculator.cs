afe1fe2c0d0625c6473fa9a41d3b76707d60dea1
kensan73 <ken.60625@gmail.com>
2015-10-15 06:14:04 -0500

    check just start and end

--- DistanceCalculator/Calculator.cs
+++ DistanceCalculator/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using RouteTraverser;
 
@@ -22,9 +23,14 @@ namespace DistanceCalculator
                 return graph[myPath];
             }
 
-            foreach (var traversal in traversals.Where(t => t.Count == myPath.Length-1))
+            foreach (var traversal in traversals.Where(t => t.Count == myPath.Length))
             {
-                    
+                if (traversal[0][0] == myPath[0] && traversal[traversal.Count - 1][1] == myPath[1])
+                    return traversal.Select(t => graph[t]).Sum();
+//                var myPathAndTraversal = myPath.Zip(traversal, (p, t) => new { Path = p, Traversal = t });
+//                var isDifferent = myPathAndTraversal.Any(nw => nw.Path != nw.Traversal[0]);
+//                if (isDifferent == false)
+//                    return graph[myPath];
             }
 
             return -1;
