3b6d17971ffa5a0925622823cbcd2ab359a09f94
kensan73 <ken.60625@gmail.com>
2015-10-15 00:09:48 -0500

    First checkin.

---
+++ RouteTraverser.Tests/WhenInvoking.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace RouteTraverser.Tests
+{
+    public partial class RouteTraverserTests
+    {
+        [TestFixture]
+        public class WhenInvoking
+        {
+            private Traverser _traverser;
+
+            [SetUp]
+            public void Setup()
+            {
+                _traverser = new Traverser();
+            }
+
+            [TearDown]
+            public void Teardown()
+            {
+                
+            }
+
+            [Test]
+            public void DepthSearchOnEmptyGraphReturnsEmptyTraversal()
+            {
+                var emptyInput = new Dictionary<string, int>();
+
+                var result = _traverser.Invoke(emptyInput);
+
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void DepthSearchOnOneNodeGraphReturnsOneTraversal()
+            {
+                var sourceDest = "AB";
+                var oneNode = new Dictionary<string, int>
+                {
+                    {sourceDest, 5}
+                };
+
+                var result = _traverser.Invoke(oneNode);
+
+                Assert.That(result.Count, Is.EqualTo(1));
+                Assert.That(result[0][0], Is.EqualTo(sourceDest));
+            }
+
+            [Test]
+            public void DepthSearchOnTwoNodeConnectedGraphReturnsOneTraversalTwoNodes()
+            {
+                const string sourceDest = "AC";
+                const string sourceDest2 = "CD";
+
+                var oneNode = new Dictionary<string, int>
+                {
+                    {sourceDest, 5},
+                    {sourceDest2, 11}
+                };
+
+                var result = _traverser.Invoke(oneNode);
+
+                Assert.That(result.Count, Is.EqualTo(2));
+                Assert.That(result[0][0], Is.EqualTo(sourceDest));
+                Assert.That(result[0][1], Is.EqualTo(sourceDest2));
+            }
+
+            //test avoids circular traversal
+            //honors early exit request?
+        }
+    }
+}
---
+++ RouteTraverser/Traverser.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RouteTraverser
+{
+    public class Traverser
+    {
+        public List<List<string>> Invoke(Dictionary<string, int> inputGraph)
+        {
+            if(!inputGraph.Any())
+                return new List<List<string>>();
+
+            if (inputGraph.Count == 1)
+                return new List<List<string>>
+                {
+                    {new List<string> {inputGraph.Keys.First()}}
+                };
+
+            return Traverse(inputGraph);
+        }
+
+        private List<List<string>> Traverse(Dictionary<string, int> inputGraph)
+        {
+            var traversals = new List<List<string>>();
+
+            foreach (var startLeg in inputGraph.Keys)
+            {
+                var traversal = new List<string>();
+                TraverseAux(startLeg, inputGraph, ref traversal);
+                traversals.Add(traversal);
+            }
+
+            return traversals;
+        }
+
+        private void TraverseAux(string startLeg, Dictionary<string, int> inputGraph, ref List<string> traversal)
+        {
+            // once you enter this function you have traversed from startLeg[0]
+            // ..to startLeg[1] so update bookkeeping here
+            traversal.Add(startLeg);
+
+            var currentLoc = startLeg[1];
+            var possibleDests = inputGraph.Keys.Where(k => k[0] == currentLoc).ToList();
+
+            foreach (var nextDest in possibleDests)
+            {
+                TraverseAux(nextDest, inputGraph, ref traversal);
+            }
+        }
+    }
+}
